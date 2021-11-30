36e8ec1812bb03f5db5fd067c49661c0840f31fc
Johnson Kwok <kwokjo2@oocl.com>
2021-11-30 21:18:12 +0800

    [POS machine] [Johnson Kwok] feat: Complete         Map<String, ReceiptItem> receiptItems = calculateReceiptItems(itemWithDetail);

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -15,7 +15,9 @@ public class PosMachine {
     }
 
     private Receipt calculateReceipt (List<ItemInfo> itemWithDetail) {
-
+        Map<String, ReceiptItem> receiptItems = calculateReceiptItems(itemWithDetail);
+        int totalPrice = calculateTotalPrice(receiptItems);
+        return new Receipt(receiptItems, totalPrice);
     }
 
     private String renderReceipt (Receipt receipt) {
